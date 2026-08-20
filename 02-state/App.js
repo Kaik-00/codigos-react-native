fdbbeead54443c7a116c49de4a95e6317d8a65dd
KAIK RIBEIRO DA SILVA BORGES <kaik.rsborges@senacsp.edu.br>
2026-08-20 12:03:41 -0300

    state e view

---
+++ 02-state/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class App extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			nome: ''
+		};
+		
+		// Deixando a função acessar as propriedades
+		this.entrar = this.entrar.bind(this)
+	}
+
+	entrar(){
+		this.setState({
+			nome: 'Ana Claudia'
+		})
+	}
+
+	render(){
+		return(
+		<View style={{marginTop: 50}}>
+			<Button title="Entrar" onPress={this.entrar}/>
+			
+			<Text style={{ fontSize: 23, color: 'orange', textAlign: 'center' }}>
+				{this.state.nome}
+			</Text>
+		</View>
+		);
+	}
+}
+
+export default App;
